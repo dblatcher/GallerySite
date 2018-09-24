5f8f125a453c1e0a8b1d545f30c2e15b9e574a97
dblatcher <dv.bltchr@gmail.com>
2018-09-24 16:53:19 +0100

    add error handling middleware and 404 response to app.

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ var siteSettings = {
 
 var gallery = getGalleries(siteSettings);
 var galleryRouter = require('./src/routes/galleryRoutes')(navBar,siteSettings);
-app.use('/gallery',galleryRouter);
 
 var watcher = fs.watch('./public/galleries',{recursive:true},
 (eventType,fileName) => {
@@ -38,6 +37,17 @@ app.get('/', function(req,res){
 
 app.get('/about', function(req,res){
 	res.render('aboutPage', {title: 'About', navBar:navBar, galleries:gallery,siteSettings:siteSettings});
+});
+
+app.use('/gallery',galleryRouter);
+
+app.use(function (req, res, next) {
+  res.status(404).render('errorPage', {errorMessage:"404 - file not found", navBar:navBar,siteSettings:siteSettings });
+})
+
+app.use(function (err, req, res, next) {
+  res.status(500)
+  res.render('errorPage', {errorMessage:err, navBar:navBar,siteSettings:siteSettings });
 });
 
 server.listen(port);
