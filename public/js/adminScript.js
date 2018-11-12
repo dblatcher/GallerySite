f70338b0017cdcebb668f47f929785ba001a2983
dblatcher <dv.bltchr@gmail.com>
2018-11-12 12:29:16 +0000

    Client waits for Gallery update response and refresh section; make server module send test data

--- public/js/adminScript.js
+++ public/js/adminScript.js
@@ -1,6 +1,5 @@
 document.getElementById('dataHolder').data = JSON.parse(document.getElementById('dataHolder').getAttribute('originalData'));
 
-
 function handleSelect(gallery) {
 	var section = document.getElementsByClassName('editSection');
 	for (i=0; i<section.length; i++) {
@@ -17,7 +16,7 @@ function handleThumbClick(element){
 function handleUploadControlClick(element){
 	var input = element.getElementsByClassName('hiddenFileInput')[0];
 	if (input.files.length === 0) {	
-		input.click()
+		input.click() // leads to handleFiles call if user selects a file.
 	} else {
 		element.parentElement.removeChild(element);
 	};
@@ -26,7 +25,6 @@ function handleUploadControlClick(element){
 function handleFiles(element) {
 	if (element.files.length === 0) {return false};
 	file=element.files[0];
-	
 	
 	if (file.type.startsWith('image/')){	
 		var img = document.createElement("img");
@@ -38,17 +36,7 @@ function handleFiles(element) {
     reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
     reader.readAsDataURL(file);
 		
-		var newUploadControl = document.createElement('span');
-		newUploadControl.setAttribute('class','thumbNail uploadControl');
-		newUploadControl.setAttribute('onclick','handleUploadControlClick(this)');
-		newUploadControl.innerHTML = "add picture";
-		var newFileInput = document.createElement('input');
-		newFileInput.setAttribute('class','hiddenFileInput');
-		newFileInput.setAttribute('type','file');
-		newFileInput.setAttribute('onchange','handleFiles(this)');
-		newUploadControl.appendChild(newFileInput);
-		element.parentElement.parentElement.appendChild(newUploadControl);
-		
+		element.parentElement.parentElement.appendChild(makeNewUploadControl());
 		return true;
 	} 
 	
@@ -64,67 +52,94 @@ function handleColorPick(element, colorType) {
 	rightInput.value = element.value;
 };
 
-function sendGalleryUpdateToServer(gallery) {
-	var data = makeUpdatedGalleryObjectFromDom(gallery);
+function sendGalleryUpdateToServer(gallery,element) {
+	if (element.classList.contains("disabled")) {return false};
+	element.classList.add("disabled");
 	
-	var newThumbCollection = document.getElementById('edit'+gallery).getElementsByClassName('added');
-	var picturesToAdd = [];
-		
-	for (var i=0; i<newThumbCollection.length; i++) {
-		picturesToAdd.push(newThumbCollection[i].children[1].file);
+	var formData = makeFormDataFromDom(gallery);
+	var uri = "galleryupdateupload";
+	var xhr = new XMLHttpRequest();
+	xhr.responseType = 'json';
+	xhr.open("POST", uri, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState !== 4) {return false};
+		alert(xhr.response.message ? xhr.response.message : "Undefined Error");
+		if (xhr.status == 200) {
+			refreshSection(gallery,xhr.response.data);
+		}
+		element.classList.remove("disabled");
 	};
+	xhr.send(formData);
 	
-	new postFilesAndData(picturesToAdd,data);
+	function refreshSection (gallery, newData) {
+		var section = document.getElementById('edit'+gallery);
+		section.children[0].innerHTML = 'edit Gallery: ' + newData.displayTitle;
+		
+		var thumbNailArea = section.getElementsByClassName('thumbNailArea')[0];
+		while (thumbNailArea.children.length > 0 ) {
+			thumbNailArea.removeChild(thumbNailArea.firstElementChild);
+		}
+		
+		for (var i = 0; i < newData.picture.length; i++){
+			thumbNailArea.appendChild(makeNewThumbNail(newData.picture[i]));
+		}
+		thumbNailArea.appendChild(makeNewUploadControl());
+		
+	}
 	
-	function makeUpdatedGalleryObjectFromDom (gallery) {
-		var data = JSON.parse(document.getElementById('dataHolder').getAttribute('originalData'))[gallery];
-		var form = document.getElementById("propertyForm"+gallery);
-		
-		data.displayTitle = form.elements.displayTitle.value;
-		data.description = form.elements.description.value;
-		data.foreground = form.elements.foreground.value;
-		data.background = form.elements.background.value;
-		
-		var deletedThumbCollection = document.getElementById('edit'+gallery).getElementsByClassName('deleted');
-		
-		var picToDelete;
-		for (var i=0; i<deletedThumbCollection.length; i++) {
-			picToDelete = deletedThumbCollection[i].children[0].src;
-			picToDelete = picToDelete.substring(picToDelete.lastIndexOf('/')+1);	
-			for (var j=0; j<data.picture.length; j++) {
-				if (data.picture[j] === picToDelete) {data.picture.splice(j,1);};
-			};
-		};
-		
-		return data;
-	};
-	
-	function postFilesAndData(files,data) {
-		var uri = "galleryupdateupload";
-		var xhr = new XMLHttpRequest();
+	function makeFormDataFromDom (gallery) {
 		var fd = new FormData();
 		
-		xhr.open("POST", uri, true);
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4 && xhr.status == 200) {
-				console.log(xhr.responseText); // handle response.
-			}
-		};
+		var oldData = JSON.parse(document.getElementById('dataHolder').getAttribute('originalData'))[gallery];
+		fd.append('title', oldData.title);
+		fd.append('main', oldData.main);
 		
-		for (var i=0; i<files.length; i++) {
-			fd.append('file_'+i, files[i]);			
-		}; 
-
-		fd.append('displayTitle', data.displayTitle);
-		fd.append('description', data.description);
-		fd.append('foreground', data.foreground);
-		fd.append('background', data.background);
-		fd.append('picture', data.picture);
+		var form = document.getElementById("propertyForm"+gallery);
+		fd.append('displayTitle', form.elements.displayTitle.value);
+		fd.append('description', form.elements.description.value);
+		fd.append('foreground', form.elements.foreground.value);
+		fd.append('background', form.elements.background.value);
 				
-		xhr.send(fd);
-	}
+		var picturesToRemove = [];
+		var deletedThumbCollection = document.getElementById('edit'+gallery).getElementsByClassName('deleted');
+		for (var i=0; i<deletedThumbCollection.length; i++) {
+			picturesToRemove.push(
+				deletedThumbCollection[i].children[0].src.substring(deletedThumbCollection[i].children[0].src.lastIndexOf('/')+1)
+			) ;
+		};
+		fd.append('picturesToRemove', picturesToRemove);
+		
+		var newThumbCollection = document.getElementById('edit'+gallery).getElementsByClassName('added');
+		for (var i=0; i<newThumbCollection.length; i++) {
+			fd.append('file_'+i, newThumbCollection[i].children[1].file);			
+		};
+				
+		return fd;
+	};
 	
 }
 
+function makeNewUploadControl() {
+	var newUploadControl = document.createElement('span');
+	newUploadControl.setAttribute('class','thumbNail uploadControl');
+	newUploadControl.setAttribute('onclick','handleUploadControlClick(this)');
+	newUploadControl.innerHTML = "add picture";
+	var newFileInput = document.createElement('input');
+	newFileInput.setAttribute('class','hiddenFileInput');
+	newFileInput.setAttribute('type','file');
+	newFileInput.setAttribute('onchange','handleFiles(this)');
+	newUploadControl.appendChild(newFileInput);
+	return newUploadControl;
+};
+
+function makeNewThumbNail(source) {
+	var newThumbNail = document.createElement('span');
+	newThumbNail.setAttribute('class','thumbNail current');
+	newThumbNail.setAttribute('onclick','handleThumbClick(this)');
+	var newImg = document.createElement('img');
+	newImg.setAttribute('src',source);
+	newThumbNail.appendChild(newImg);
+	return newThumbNail;	
+}
 
 handleSelect(0);
--- src/JS/handleGalleryUpdateModule.js
+++ src/JS/handleGalleryUpdateModule.js
@@ -1,30 +1,72 @@
+var wrapper = function (currentGalleryData){
+	var fs = require("fs");
+	const delberateFail = false;
 
-var handler = function(request, response){	
-	console.log('---files and data incoming---');
-	console.log("We got data about " + request.fields.displayTitle);
+	var handler = function(request, response){	
+		console.log('---files and data incoming---');
+		
+		if (delberateFail) {
+			console.log( 'deliberate fail for testing' );
+			response.status(500);
+			response.send({success:false, message:"test fail"});
+			return false;
+		};
+		
 	
-	var fileKeys = [];
-	if (request.files) {fileKeys = Object.keys(request.files)};
-	
-	fileKeys.forEach(function(key){
-		var file = request.files[key];
-		console.log('name: ' + file.name + ' ');
-		/*file.mv('uploaded/'+file.name, function(err) {
-			if (err) {
-				console.log(err);
-				return response.status(500).send(err);
-			} else {
-				console.log(file.name + ' uploaded');
-			}
-		});	*/
-	});
-	
-	response.send('All '+ fileKeys.length +' files received!');	
-	console.log('--done--');
+		console.log("current galleries =  " + currentGalleryData.length);
+		console.log("server received data about " + request.fields.title);
+		var fileKeys = [];
+		if (request.files) {fileKeys = Object.keys(request.files)};
+		console.log("new files:");
+		fileKeys.forEach(function(key){
+			var file = request.files[key];
+			console.log('name: ' + file.name + ' ');
+		});
+		console.log("picture to remove:");
+		console.log(request.fields.picturesToRemove);
+		console.log('------');
 
+		
+		let picPath = function(picFileName) {
+			return '../galleries/' + request.fields.title + '/' + picFileName;
+		};
+		
+		// TO DO
+		//verify request.fields.title is a real gallery subfolder
+		// respond with fail message if not
+		// handle the 'main' property
+		
+		// TO DO
+		//Promise.All:
+		//	write updated info.JSON file and save in subfolder
+		//	use mv method to copy file objects to subfolder
+		//	create 'archive' subfolder in gallery subfolder (if not there already)
+		//	move picturesToRemove to archive folder
+		//then:
+		//	update currentGalleryData[x] 
+		//	response.send revised data on
+		//remove dummy data sending
+		
+		//check for conflicts caused by the app level watcher - remove? 
+		
+		var revisedGallery = {
+			displayTitle: "fooBar",
+			main:0,
+			picture:[picPath("cat.gif")]
+		}
+		
+		response.send({
+			success:true, 
+			message:"Test success message with dummy data",
+			data: revisedGallery
+		});
+
+		
+	
+	}
+
+
+	return handler;
 }
 
-
-
-
-module.exports = handler;
+module.exports = wrapper;
